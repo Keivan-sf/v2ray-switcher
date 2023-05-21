fdc869ba8ef8e0f63b0b21e24f2679f3d79b0576
Keivan-sf <keyvan0082@gmail.com>
2023-05-21 18:49:26 +0330

    feat: get subscription links from `subscription.txt`

--- src/Handler/utils/CommandHandlers/commands/index.ts
+++
@@ -1,2 +0,0 @@
-export const singleCommands = ["start"] as const;
-export const commandsWithArgs = ["add"] as const;
--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,12 @@
-import { CommandHandler } from "./Handler";
-process.stdin.on("data", CommandHandler.onMessage);
+import fs from "fs/promises";
+
+async function start() {
+    const subs_file = await fs.readFile("./subscriptions.txt");
+    const sub_links = subs_file.toString().split("\n").filter(l => l);
+    if(sub_links.length < 1) {
+        throw new Error("No sub links");
+    }
+    console.log('links: ' , sub_links)
+}
+
+start();
