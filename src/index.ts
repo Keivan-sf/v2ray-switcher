5487f2b171de74300b2aa6845c2f3740034be31a
Keivan-sf <keyvan0082@gmail.com>
2023-05-28 16:02:52 +0330

    chore: add v2ray core

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,15 @@
 import { Files } from "./utils/Files";
 import { Subscription } from "./utils/SubscriptionServerExtractor";
+import { V2rayJsonConfig } from "./utils/SubscriptionServerExtractor/interfaces";
 
 async function start() {
     const files = new Files();
     const sub_links = await files.getSubscriptionLinks("./subscriptions.txt");
+    const configs: V2rayJsonConfig[] = [];
     for (const link of sub_links) {
-        console.dir(await new Subscription(link).getJsonConfigs(), {
-            depth: 10,
-        });
+        configs.push(...(await new Subscription(link).getJsonConfigs()));
     }
+    console.dir(configs, { depth: 10 });
 }
 
 start();
