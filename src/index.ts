1bc2375c39b9e9e8d4e05feb952fbb6fe60cf961
Keivan-sf <keyvan0082@gmail.com>
2023-05-28 18:36:44 +0330

    feat: run v2ray servers using core

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,7 @@
 import { Files } from "./utils/Files";
 import { Subscription } from "./utils/SubscriptionServerExtractor";
 import { V2rayJsonConfig } from "./utils/SubscriptionServerExtractor/interfaces";
+import { ServerTester } from "./utils/ServerTester/index";
 
 async function start() {
     const files = new Files();
@@ -9,7 +10,11 @@ async function start() {
     for (const link of sub_links) {
         configs.push(...(await new Subscription(link).getJsonConfigs()));
     }
-    console.dir(configs, { depth: 10 });
+    // console.dir(configs, { depth: 10 });
+    const tester = new ServerTester(4080);
+    await tester.run(configs[0]);
+    console.log("started resolved");
+    console.dir(configs[0], { depth: 10 });
 }
 
 start();
--- src/utils/ServerTester/index.ts
+++ src/utils/ServerTester/index.ts
@@ -1,8 +1,52 @@
+import { Files } from "../Files";
 import { V2rayJsonConfig } from "../SubscriptionServerExtractor/interfaces";
+import treeKill from "tree-kill";
+import * as $ from "node:child_process";
+import { SocksProxyAgent } from "socks-proxy-agent";
+import axios from "axios";
 
-class Server {
-    constructor(config: V2rayJsonConfig, public port: number){}
-    public test(){
-               
+export class ServerTester {
+    private files: Files = new Files();
+    public connection_status: "connected" | "disconnected" = "disconnected";
+    constructor(public port: number) {}
+    public run = (config: V2rayJsonConfig) =>
+        new Promise<void>(async (resolve, reject) => {
+            this.setPortToConfig(config);
+            const config_path = await this.files.createJsonFile(
+                `${this.port}`,
+                config
+            );
+            const command = `${__dirname}/v2ray-core/v2ray run --config=${config_path}`;
+            const cmd = $.spawn(command, { shell: true });
+            cmd.stdout.on("data", async (data: Buffer) => {
+                const out = data.toString();
+                console.log(out);
+                if (!out.includes("started")) return;
+                try {
+                    console.log("Server started");
+                    resolve();
+                    if (cmd.pid) treeKill(cmd.pid);
+                } catch (err) {
+                    console.log(err);
+                }
+            });
+        });
+    private setPortToConfig(config: V2rayJsonConfig) {
+        config.inbounds = [
+            {
+                port: this.port,
+                protocol: "SOCKS",
+                settings: {
+                    auth: "noauth",
+                    udp: true,
+                    userLevel: 8,
+                },
+                sniffing: {
+                    destOverride: ["http", "tls"],
+                    enabled: true,
+                },
+                tag: "socks",
+            },
+        ];
     }
 }
