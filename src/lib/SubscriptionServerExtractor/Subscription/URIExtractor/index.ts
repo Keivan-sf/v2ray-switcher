ee93af76fbb1c724d3053ddb806dc0189050a8c1
Keivan-sf <keyvan0082@gmail.com>
2023-09-21 11:44:38 +0330

    feat: support vless servers and non-base64 subscriptions

--- src/lib/SubscriptionServerExtractor/Subscription/URIExtractor/index.ts
+++ src/lib/SubscriptionServerExtractor/Subscription/URIExtractor/index.ts
@@ -1,5 +1,6 @@
 import axios from "axios";
 import { VmessURI } from "./URITypes/Vmess";
+import { VlessURI } from "./URITypes/Vless";
 import { ConfigURI } from "../../../interfaces";
 
 export class URIExtractor {
@@ -7,13 +8,21 @@ export class URIExtractor {
         const source_code = await this.getSourceCode(link);
         const servers = source_code
             .split("\n")
-            .filter((l) => l && l.startsWith("vmess"))
-            .map((l) => l.replace("\r", ""));
-        return servers.map((s) => new VmessURI(s));
+            .reduce((acc , l) => {
+                if(!l) return acc;
+                if(l.startsWith("vmess://")) acc.push(new VmessURI(l))
+                if(l.startsWith("vless://")) acc.push(new VlessURI(l))
+                return acc;
+            }, [] as ConfigURI[])
+        return servers;
     }
     private async getSourceCode(url: string) {
-        const source_base64 = (await axios.get(url)).data;
-        const source = Buffer.from(source_base64, "base64").toString();
-        return source;
+        const raw_source_code = (await axios.get(url)).data;
+        // checking to see if the source code is base-64 or not
+        if (!raw_source_code.includes("://")) {
+            return Buffer.from(raw_source_code, "base64").toString();
+        } else {
+            return Buffer.from(raw_source_code).toString();
+        }
     }
 }
