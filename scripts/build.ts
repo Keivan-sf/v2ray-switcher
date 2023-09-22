95d79bda40c45c1f81218fd6679a54427edae207
Keivan-sf <keyvan0082@gmail.com>
2023-09-22 17:37:02 +0330

    chore: create servers.txt in build script

--- scripts/build.ts
+++ scripts/build.ts
@@ -2,6 +2,9 @@ import fs from "fs";
 import path from "path";
 const configs_dir = path.join(__dirname, "..", "src/lib/Files/.configs");
 const subscription_file = path.join(__dirname, "..", "subscriptions.txt");
+const servers_file = path.join(__dirname, "..", "servers.txt");
 if (!fs.existsSync(configs_dir)) fs.mkdirSync(configs_dir);
 if (!fs.existsSync(subscription_file)) fs.writeFileSync(subscription_file, "");
+if (!fs.existsSync(servers_file)) fs.writeFileSync(servers_file, "");
+
 console.log("Build finished successfully");
