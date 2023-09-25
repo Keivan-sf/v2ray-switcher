845ca5b2a39c821e884346c80e37c4674c83abfc
Keivan-sf <keyvan0082@gmail.com>
2023-09-25 16:50:32 +0330

    chore: move `.configs` to project root directory

--- src/lib/Files/index.ts
+++ src/lib/Files/index.ts
@@ -1,7 +1,8 @@
 import fs from "fs/promises";
+import { getRootDir } from "../../utils/dirname";
 
 export class Files {
-    public rootDir: string = __dirname;
+    public rootDir: string = getRootDir();
     public async getSubscriptionLinks(path: string) {
         const subs_file = await fs.readFile(path);
         const sub_links = subs_file
@@ -14,7 +15,7 @@ export class Files {
         return sub_links;
     }
     public async createJsonFile(name: string, config: any) {
-        const file_path = `${__dirname}/.configs/${name}.json`;
+        const file_path = `${this.rootDir}/.configs/${name}.json`;
         await fs.writeFile(file_path, JSON.stringify(config));
         return file_path;
     }
