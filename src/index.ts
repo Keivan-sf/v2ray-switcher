df52271255a795629675d609a54cf2948228e724
Keivan-sf <keyvan0082@gmail.com>
2023-09-26 12:41:04 +0330

    chore: move root config files to src

--- src/index.ts
+++ src/index.ts
@@ -6,9 +6,9 @@ import path from "path";
 import fs from "fs";
 
 function generateEssentialDirectories() {
-    const configs_dir = path.join(__dirname, "..", ".configs");
-    const subscription_file = path.join(__dirname, "..", "subscriptions.txt");
-    const servers_file = path.join(__dirname, "..", "servers.txt");
+    const configs_dir = path.join(__dirname, ".configs");
+    const subscription_file = path.join(__dirname, "subscriptions.txt");
+    const servers_file = path.join(__dirname, "servers.txt");
     if (!fs.existsSync(configs_dir)) fs.mkdirSync(configs_dir);
     if (!fs.existsSync(subscription_file))
         fs.writeFileSync(subscription_file, "");
@@ -16,10 +16,10 @@ function generateEssentialDirectories() {
 }
 
 async function start() {
-    setRootDir(path.resolve(__dirname, ".."));
+    setRootDir(path.resolve(__dirname));
     generateEssentialDirectories();
     const files = new Files();
-    const sub_links = await files.getSubscriptionLinks("./subscriptions.txt");
+    const sub_links = await files.getSubscriptionLinks(path.join(__dirname, "subscriptions.txt"));
     const extractor = new ConfigExtractor(sub_links, 30 * 60 * 1000);
     await extractor.startExtracting();
     const switcher = new Switcher(extractor);
