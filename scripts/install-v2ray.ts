29f87f75b701d89b58964c22bb5d8e5299f23b12
Keivan-sf <keyvan0082@gmail.com>
2023-09-27 21:11:29 +0330

    feat: add caching for v2ray-core installation resolves #23

--- scripts/install-v2ray.ts
+++ scripts/install-v2ray.ts
@@ -1,10 +1,13 @@
 import os from "os";
+import fs from "fs";
 import path from "path";
 import axios from "axios";
 import shelljs from "shelljs";
 import AdmZip from "adm-zip";
 import minimist from "minimist";
 const args = minimist(process.argv.slice(2));
+
+if (!fs.existsSync(".cache")) fs.mkdirSync(".cache");
 
 const DOWNLOAD_LINKS: { [k in string]: string } = {
     "linux-x64":
@@ -22,15 +25,24 @@ function getTarget() {
 }
 
 async function installV2rayBinaries(download_url: string, outdir: string) {
-    const v2ray_zip_buffer: Buffer = (
-        await axios({
-            url: download_url,
-            responseType: "arraybuffer",
-        })
-    ).data;
+    const file_name = download_url.split("/").at(-1);
+    let v2ray_zip_buffer: Buffer;
+    if (!fs.existsSync(`.cache/${file_name}`)) {
+        console.log("Cache not found, downloading")
+        v2ray_zip_buffer = (
+            await axios({
+                url: download_url,
+                responseType: "arraybuffer",
+            })
+        ).data;
+        fs.writeFileSync(`.cache/${file_name}`, v2ray_zip_buffer);
+    } else {
+        console.log("Using cached version")
+        v2ray_zip_buffer = fs.readFileSync(`.cache/${file_name}`);
+    }
     const zip = new AdmZip(v2ray_zip_buffer);
-    zip.extractAllTo(path.resolve(outdir, "v2ray-core"), true);
-    shelljs.chmod("+x", path.resolve(outdir , "v2ray-core/v2ray"));
+    zip.extractAllTo(path.join(outdir, "v2ray-core"), true);
+    shelljs.chmod("+x", path.join(outdir, "v2ray-core/v2ray"));
 }
 
 async function start() {
