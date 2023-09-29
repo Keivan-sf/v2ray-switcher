1936fe4a24c6ca48bc88e29aaf6dfc0736ef3db7
Keivan-sf <keyvan0082@gmail.com>
2023-09-29 17:54:27 +0330

    chore: improve config file parser error messages

--- src/lib/ConfigParser/index.ts
+++ src/lib/ConfigParser/index.ts
@@ -1,5 +1,6 @@
 import * as z from "zod";
 import fs from "fs/promises";
+import { ZodError } from "zod";
 const configSchema = z.object({
     subscription_urls: z.array(z.string()).default([]),
     servers: z.array(z.string()).default([]),
@@ -15,12 +16,29 @@ type ConfigSchema = z.infer<typeof configSchema>;
 export const parseConfig = async (file_path: string): Promise<ConfigSchema> => {
     const file_content = await fs.readFile(file_path);
     const json = JSON.parse(file_content.toString());
-    const parsed = configSchema.parse(json);
-    if (parsed.servers.length === 0 && parsed.subscription_urls.length === 0) {
-        console.log(
-            "There are no servers nor subscription URIs in `config.json`"
-        );
-        process.exit(0);
+    try {
+        const parsed = configSchema.parse(json);
+        if (
+            parsed.servers.length === 0 &&
+            parsed.subscription_urls.length === 0
+        ) {
+            console.log(
+                "There are no servers nor subscription URIs in `config.json`"
+            );
+            process.exit(0);
+        }
+        return parsed;
+    } catch (err: any) {
+        if (err.issues) {
+            const error = err as ZodError;
+            const message = error.errors
+                .map((e) => e.path.join(" > ") + ": " + e.message)
+                .join("\n");
+            console.log("There are erros in your configuration file:");
+            console.log(message);
+            process.exit(0);
+        } else {
+            throw err;
+        }
     }
-    return parsed;
 };
