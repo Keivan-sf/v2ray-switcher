d147917cc35cc3dcf6e8f4f39fd6fd376166be68
Keivan-sf <keyvan0082@gmail.com>
2023-09-30 15:28:44 +0330

    chore: remove extra command from build script

--- scripts/build.ts
+++ scripts/build.ts
@@ -6,7 +6,6 @@ fs.rmSync(".build", { recursive: true, force: true });
 
 executeCmd("npx tsc")
 executeCmd("npx ncc build .dist/index.js -o .build")
-executeCmd("npx ncc build .dist/index.js -o .build")
 executeCmd("npx pkg .build/index.js -t node18-linux-x64,node18-macos-x64,node18-win-x64 --out-path .build")
 
 fs.rmSync(".dist", { recursive: true, force: true });
